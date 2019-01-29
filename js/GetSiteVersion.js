dde4465c684c45c3bd30678139dccfe323acd00e
JoshuaA-WCCC <46837023+JoshuaA-WCCC@users.noreply.github.com>
2019-01-28 22:58:12 -0800

    Add files via upload
    Script to log the site version to the browser console

---
+++ js/GetSiteVersion.js
@@ -0,0 +1,20 @@
+// get the results from /store/sitemonitor.aspx and parse into array and output the version of the site
+
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+$.get('https://secure.cougarpartscatalog.com/store/sitemonitor.aspx',function(data){
+var site_status = getQueryParams(data);
+console.log("Site Version: "+site_status['Code Version']);
+});
